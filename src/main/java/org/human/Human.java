cf68377a1726f4fd4f838c81d320f3ddc7ed02f4
Kani-sensei <kani.autor@gmail.com>
2024-02-17 16:47:21 +0100

    add:birthday methods

--- src/main/java/org/human/Human.java
+++ src/main/java/org/human/Human.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Human {
     private String name;
     private String gender;
-    private int age;
+    private static int age;
 
     // Constructor
 
@@ -35,9 +35,27 @@ public class Human {
         this.age = age;
     }
 
-    public static boolean haveBirthday(LocalDate birthdate) {
+    public static void haveBirthday(LocalDate birthdate) {
         LocalDate today = LocalDate.now();
-        return today.getMonthValue() == birthdate.getMonthValue() && today.getDayOfMonth() == birthdate.getDayOfMonth();
+        if (today.getMonthValue() == birthdate.getMonthValue() && today.getDayOfMonth() == birthdate.getDayOfMonth()) {
+            age++;
+            System.out.println("Happy birthday! You are now " + age + " years old!");
+        }
+        else {
+            System.out.println("Sorry, it's not your birthday!");
+            int untilBirthdayMonth = birthdate.getDayOfMonth() - today.getDayOfMonth();
+            int untilBirthdayDay = birthdate.getDayOfMonth() - today.getDayOfMonth();
+            if (untilBirthdayMonth < 1 || untilBirthdayMonth > 12 ) {
+                throw new IllegalArgumentException("Unknown number of Month: " + untilBirthdayMonth);
+            }
+            else if (untilBirthdayDay < 1) {
+                throw new IllegalArgumentException("It must above 1 and not " + untilBirthdayDay);
+            }
+            else {
+                System.out.println("You have " + untilBirthdayMonth + " Months and " + untilBirthdayDay + " Days until your birthday!");
+            }
+        }
+
     }
 
     public static LocalDate createBirthdate(int year, int month, int day) {
